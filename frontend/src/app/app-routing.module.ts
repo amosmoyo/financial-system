11f57eda6afbdc857b285a633ac7edd7bd53118c
amosmoyo <amosmoyo5300@gmail.com>
2021-02-08 18:51:08 +0300

    setting navigatio

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { Notfound404Component } from './notfound404/notfound404.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+		path: 'admin',
+		loadChildren: () => import('./main/main/main.module').then((m) => m.MainModule)
+	},
+  {
+    path:'', redirectTo: 'login', pathMatch: 'full',
+  },
+  {
+    path: '**', component:Notfound404Component
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [LoginComponent, Notfound404Component],
+  imports: [RouterModule.forRoot(routes), FormsModule, CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
